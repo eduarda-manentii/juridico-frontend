5de207b33e664b4fab37fcdb9b4913648fd6b6cc
eduada-manentii <dudamanenti65@gmail.com>
2025-08-06 10:39:56 -0300

    Importado boostrap ao projeto.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    importProvidersFrom(HttpClientModule),
     provideToastr(),
     provideAnimations()
   ]
-
 };

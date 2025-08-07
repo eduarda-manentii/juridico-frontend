935b53700578511e28145451e2bba9e8ef7d368b
= <=>
2025-08-07 12:39:02 -0300

    Implementado Bearer Token.

--- src/app/shared/services/backend-api.service.ts
+++ src/app/shared/services/backend-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
 
 @Injectable({
@@ -11,6 +11,13 @@ export class BackendApiService {
 
   constructor(private http: HttpClient) {}
 
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
   register(data: { nome: string; email: string; senha: string }): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/usuarios`, data, { responseType: 'text' as 'json' });
   }
@@ -19,23 +26,27 @@ export class BackendApiService {
     return this.http.patch(`${this.baseUrl}${endpoint}`, { params });
   }
 
-  get(endpoint: string, params?: any): Observable<any> {
-    return this.http.get(`${this.baseUrl}${endpoint}`, { params });
+   get(endpoint: string, params?: any): Observable<any> {
+    const headers = this.getAuthHeaders();
+    return this.http.get(`${this.baseUrl}${endpoint}`, { headers, params });
   }
 
   delete(endpoint: string): Observable<any> {
-    return this.http.delete(`${this.baseUrl}${endpoint}`, { responseType: 'text' });
+    const headers = this.getAuthHeaders();
+    return this.http.delete(`${this.baseUrl}${endpoint}`, { headers, responseType: 'text' });
   }
 
   create(endpoint: string, data: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}${endpoint}`, data, { responseType: 'text' as 'json' });
+    const headers = this.getAuthHeaders();
+    return this.http.post(`${this.baseUrl}${endpoint}`, data, { headers, responseType: 'text' as 'json' });
   }
 
   update(endpoint: string, data: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}${endpoint}`, data, { responseType: 'text' as 'json' });
+    const headers = this.getAuthHeaders();
+    return this.http.put(`${this.baseUrl}${endpoint}`, data, { headers, responseType: 'text' as 'json' });
   }
 
-  login(data: { email: string; senha: string }): Observable<any> {
+   login(data: { email: string; senha: string }): Observable<any> {
     return this.http.post(`${this.baseUrl}/usuarios/login`, data);
   }
 
